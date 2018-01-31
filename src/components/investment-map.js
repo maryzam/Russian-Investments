7f8e52b8350c551ade083b2a737e36fa3127baff
Mary <zamo.code@gmail.com>
2018-02-01 02:22:01 +0300

    Colorize regions using info financial stats (draft commit)

--- src/components/investment-map.js
+++ src/components/investment-map.js
@@ -7,6 +7,13 @@ import * as topojson from "topojson";
 const sizeRatio = 0.5;
 const mapScale = 0.7;
 const offset = 50;
+const defaultInfo = { Receive: 0, Invest: 0, Delta: 0 };
+
+const arrayToObject = (array, key) =>
+    array.reduce((obj, item) => {
+            obj[item[key]] = item;
+            return obj;
+        }, {});
 
 class InvestmentMap {
 
@@ -22,8 +29,9 @@ class InvestmentMap {
             .rotate([-105, 0])
             .center([-10, 65])
             .scale(scale)
-            .translate([width / 2, height / 2 ]);
+            .translate([width / 2, height / 2]);
 
+        this.year = "2009";
         this.path = d3.geoPath().projection(projection);
         this.svg = $container
                         .append("svg")
@@ -38,17 +46,37 @@ class InvestmentMap {
             .await(renderMap);
     }
 
-    init(geo, data) {
+    init(geo, finance) {
 
         const regions = topojson.feature(geo, geo.objects.regions).features;
+        const investments = finance.Years.find((d) => (d.Year + "" === this.year));
+        const financeLookup = arrayToObject(investments.Regions, "RegionCode");
 
+        const max = investments.Regions.reduce((res, curr) => Math.max(res, curr.Delta), 0);
+        const min = investments.Regions.reduce((res, curr) => Math.min(res, curr.Delta), 0);
+        const colorScale = d3.scaleLinear()
+                                .domain([max, 0, min])
+                                .range(["#d73027", "#fee08b", '#1a9850'])
+                                .interpolate(d3.interpolateHcl);
+
+        regions.forEach((r) => {
+            let info = financeLookup[r.properties.OKATO];
+            if (!info) {
+                info = defaultInfo;
+            }
+                r.properties["receive"] = info.Receive;
+                r.properties["invest"] = -info.Invest;
+                r.properties["delta"] = info.Delta;
+        });
+     
         this.svg
             .append("g")
             .selectAll("path")
             .data(regions)
                 .enter()
                     .append("path")
-                    .attr("d", this.path);
+                        .attr("d", this.path)
+                        .style("fill", (d) => { return colorScale(d.properties.delta)});
     }
 }
 
